faa465f97ed3563c274b34abfd2fdebed8885245
Sid541 <144512053+Sid541@users.noreply.github.com>
2024-08-01 23:32:36 +0530

    Update MyUserController.ts

--- backend/src/controllers/MyUserController.ts
+++ backend/src/controllers/MyUserController.ts
@@ -1,10 +1,27 @@
 import { Request, Response } from "express";
 import User from "../models/user";
 
+const getCurrentUser = async (req: Request, res: Response) => {
+  try {
+    const currentUser = await User.findOne({ _id: req.userId });
+    if (!currentUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+     res.json(currentUser);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Something went wrong" });
+  }
+};
+
 
 const createCurrentUser = async (req: Request, res: Response) => {
   try {
     const { auth0Id,name, email} = req.body;
+    console.log(auth0Id);
+    console.log(name);
+    console.log(email);
     const existingUser = await User.findOne({ auth0Id });
 
     if (existingUser) {
@@ -13,6 +30,7 @@ const createCurrentUser = async (req: Request, res: Response) => {
 
     const newUser = new User(req.body);
     await newUser.save();
+    console.log(newUser)
 
     res.status(201).json(newUser.toObject());
   } catch (error) {
@@ -21,9 +39,33 @@ const createCurrentUser = async (req: Request, res: Response) => {
   }
 };
 
+const updateCurrentUser= async (req: Request, res: Response)=>{
+  try {
+      const {name, addressLine1, country, city}=req.body;
+      const user = await User.findById(req.userId)
+
+      if(!user){
+        return res.status(404).json({message: "User not found"});
+      }
+
+      
+        user.name=name;
+        user.addressLine1=addressLine1;
+        user.country=country;
+        user.city=city;
+      
+        await user.save();
+        res.send(user);
+
+  } catch (error) {
+    res.status(500).json({ message: "Error creating user" });
+  }
+}
 
 
 
 export default {
   createCurrentUser,
+  updateCurrentUser,
+  getCurrentUser,
 };
